bcad0055ea30d56467080eebda01f2bbfc274c84
abhi9704 <abhipawar0649@gmail.com>
2025-01-29 11:49:22 +0530

    Update generate_key.sh

--- generate_key.sh
+++ generate_key.sh
@@ -1,1 +1,16 @@
+#check you have a key or not
+ls -al ~/.ssh
 
+#if not, generate new
+
+ssh-keygen -t ed25519 -C "your-email@example.com"
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_ed25519
+
+cat ~/.ssh/id_ed25519.pub
+
+# Go to GitHub → Settings → SSH and GPG keys
+# Click New SSH Key
+# Paste the copied key and save.
